12f8d1b936cf5d71e8874cbc63cf05c95e6b9ccc
hammadzahidali <hammad.zahid@outlook.com>
2016-02-01 23:43:51 +0500

    new

---
+++ Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject []prefabs;
+	public float delay =2.0f;
+	public bool active=true;
+	public Vector2 delayRange= new Vector2(1,2);
+
+
+	// Use this for initialization
+	void Start () {
+		ResetDelay ();
+		StartCoroutine (EnemyGenerater());
+	}
+
+	void ResetDelay()
+	{
+		delay = Random.Range (delayRange.x,delayRange.y);
+	}
+
+	IEnumerator EnemyGenerater()
+	{
+		yield return new WaitForSeconds (delay);
+
+		if(active)
+		{
+			var newtransform= transform;
+			Instantiate(prefabs[Random.Range(0,prefabs.Length)],newtransform.position,Quaternion.identity);
+			ResetDelay();
+		}
+		StartCoroutine (EnemyGenerater());
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Scripts/TilesBG.cs
+++ Scripts/TilesBG.cs
@@ -2,20 +2,22 @@
 using System.Collections;
 
 public class TilesBG : MonoBehaviour {
+	
+	public int textureSize = 32;
+	public bool scaleHorizontially = true;
+	public bool scaleVertically = true;
 
-	public int TextureSize = 32;
-	public bool scaleVertical;
-	public bool scaleHorizontal;
+
 
 	// Use this for initialization
 	void Start () {
-		var newWidth = !scaleHorizontal? 1: Mathf.Ceil(Screen.width/ PixelsPerfectCamera.pixelperunit * TextureSize );
-		var newHeight = !scaleVertical? 1 : Mathf.Ceil(Screen.height/ PixelsPerfectCamera.pixelperunit * TextureSize );
-
-		transform.localScale = new Vector3(newWidth * TextureSize, newHeight * TextureSize , 1);
-		GetComponent<Renderer> ().material.mainTextureScale = new Vector3 (newWidth, newHeight,1);
-
+		
+		var newWidth = !scaleHorizontially ? 1 : Mathf.Ceil (Screen.width / (textureSize * PixelsPerfectCamera.scale));
+		var newHeight = !scaleVertically ? 1 : Mathf.Ceil (Screen.height / (textureSize * PixelsPerfectCamera.scale));
+		
+		transform.localScale = new Vector3 (newWidth * textureSize, newHeight * textureSize, 1);
+		
+		GetComponent<Renderer> ().material.mainTextureScale = new Vector3 (newWidth, newHeight, 1);
 	}
-
-
+	
 }
